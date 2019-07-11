b7ddd9dd5cb21db00fe1b5090c41d7b5a6c66c59
giacomocherubini <giacomo.cherubini1@gmail.com>
2019-07-11 15:40:53 +0200

    Index 1.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   </head>
   <body>
+    @include('partials._navbar')
     @yield('content')
+    <script src="{{ asset('js/app.js') }} "></script>
   </body>
 </html>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,9 +1,40 @@
-<h1>Laptops</h1>
+@extends('layouts.app')
 
-@forelse ($laptops as $laptop)
-  id: {{ $laptop->id }} - Marca: {{ $laptop->Marca}} - Modello: {{ $laptop->Modello }}
-  - prezzo: {{ $laptop->prezzo }}
-  <br>
-@empty
-  <p>Non ci sono prodotti</p>
-@endforelse
+{{-- se è qualcosa di corto posso usare anche questa forma qui senza @endsection --}}
+@section('page_title', 'Index di tutti i laptops')
+
+@section('content')
+<div class="container mt-5">
+  <h1>Laptops</h1>
+
+  <table class="table">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Marca</th>
+      <th>Modello</th>
+      <th>Prezzo</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    @forelse ($laptops as $laptop)
+      <tr>
+        <td>{{ $laptop->id }}</td>
+        <td>{{ $laptop->Marca}}</td>
+        <td>{{ $laptop->Modello }}</td>
+        <td>{{ $laptop->prezzo }}</td>
+      </tr>
+      @empty
+        <p>Non ci sono prodotti</p>
+    @endforelse
+
+
+
+  </tbody>
+</table>
+
+
+</div>
+
+@endsection
